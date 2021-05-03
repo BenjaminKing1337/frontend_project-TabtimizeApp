6398be7f056e3c0ccef829f74f4fb4ba94dee890
SamoK99 <56873602+SamoK99@users.noreply.github.com>
2021-05-03 14:53:05 +0200

    test

--- src/App.vue
+++ src/App.vue
@@ -1,55 +1,30 @@
 <template>
   <v-app>
-    <v-app-bar
-      app
-      color="primary"
-      dark
-    >
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
-
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
-
-      <v-spacer></v-spacer>
-
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
-    </v-app-bar>
-
-    <v-main>
+    <v-main class="body">
+      <NavDesktop/>
+      <!-- <NavMobile/> -->
       <router-view/>
     </v-main>
   </v-app>
 </template>
 
 <script>
+import NavDesktop from "./components/Navigation/NavDesktop";
+// import NavMobile from "./components/Navigation/NavMobile";
 
 export default {
-  name: 'App',
-
-  data: () => ({
-    //
-  }),
+  components: {
+    NavDesktop: NavDesktop,
+    // NavMobile: NavMobile,
+  }
 };
 </script>
+
+<style lang="scss">
+@import "./src/styles/styles.scss";
+
+.body{
+  background: map-get($ColorScheme, bg);
+}
+  
+</style>
