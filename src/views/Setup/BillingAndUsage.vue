b6375453ab44e746db432b4a588c74af67018eeb
SamoK99 <56873602+SamoK99@users.noreply.github.com>
2021-05-09 23:30:36 +0200

    Update BillingAndUsage.vue

--- src/views/Setup/BillingAndUsage.vue
+++ src/views/Setup/BillingAndUsage.vue
@@ -1,58 +1,61 @@
 <template>
   <div>
     <div class="main">
-      <div class="top">
-        <div class="info-box">
-          <div class="info-top">
-            <h4 class="info-title">ANALYSIS BALANCE</h4>
-          </div>
-          <div class="info-bot">
-            <div class="icon">
-              <v-icon class="balance">mdi-scale-balance</v-icon>
+        <div class="top">
+
+            <div class="info-box">
+                <div class="info-top">
+                    <h4 class="info-title">ANALYSIS BALANCE</h4>
+                </div>
+                <div class="info-bot">
+                    <div class="icon">
+                        <v-icon class="balance">mdi-scale-balance</v-icon>
+                    </div>
+                    <div class="amount">
+                        <span>0 / 5 used</span>
+                    </div>
+                    <div class="more-button">
+                        <button class="get-more">Get more
+                            <v-icon class="moar">mdi-menu-down</v-icon>
+                        </button>
+                    </div>
+                </div>
             </div>
-            <div class="amount">
-              <span>0 / 5 used</span>
+
+            <div class="info-box">
+                <div class="info-top">
+                    <h4 class="info-title">REGISTERED PAGES</h4>
+                </div>
+                <div class="info-bot">
+                    <div class="icon">
+                        <v-icon class="clip">mdi-clipboard-text</v-icon>
+                    </div>
+                    <div class="amount">
+                        <span>0 / 8 registered</span>
+                    </div>
+                    <div class="more-button"></div>
+                </div>
             </div>
-            <div class="more-button">
-              <button class="get-more">Get more
-                  <v-icon class="moar">mdi-menu-down</v-icon>
-              </button>
+
+            <div class="info-box">
+                <div class="info-top">
+                    <h4 class="info-title">LINK REQUESTS</h4>
+                </div>
+                <div class="info-bot">
+                    <div class="icon">
+                        <v-icon class="external">mdi-open-in-new</v-icon>
+                    </div>
+                    <div class="amount">
+                        <span>0 / 8 reg.</span>
+                    </div>
+                    <div class="more-button">
+                        <button class="get-more">Get more
+                            <v-icon class="moar">mdi-menu-down</v-icon>
+                        </button>
+                    </div>
+                </div>
             </div>
-          </div>
         </div>
-        <div class="info-box">
-          <div class="info-top">
-            <h4 class="info-title">REGISTERED PAGES</h4>
-          </div>
-          <div class="info-bot">
-            <div class="icon">
-              <v-icon class="clip">mdi-clipboard-text</v-icon>
-            </div>
-            <div class="amount">
-              <span>0 / 8 registered</span>
-            </div>
-            <div class="more-button"></div>
-          </div>
-        </div>
-        <div class="info-box">
-          <div class="info-top">
-            <h4 class="info-title">LINK REQUESTS</h4>
-          </div>
-          <div class="info-bot">
-            <div class="icon">
-              <v-icon class="external">mdi-open-in-new</v-icon>
-            </div>
-            <div class="amount">
-              <span>0 / 8 reg.</span>
-            </div>
-            <div class="more-button">
-                <button class="get-more">Get more
-                    <v-icon class="moar">mdi-menu-down</v-icon>
-                </button>
-            </div>
-          </div>
-        </div>
-      </div>
 
       <div class="mid">
          <div class="big-box">
@@ -76,11 +79,13 @@
                 <button class="plan-button">Change Plan</button>
             </div>
          </div>
+
           <div class="big-box">
              <div class="big-title">
                  <h3>BILLING INFORMATION</h3>
              </div>
          </div>
+         
           <div class="big-box">
              <div class="big-title">
                  <h3>PAYMENT METHOD</h3>
@@ -100,16 +105,20 @@
               <hr>
               <div class="invoice-info">
                   <div class="date">
-                      <span>Date</span>
+                      <span class="font-weight-medium">Date</span>
+                      <span>27.10.2020</span>
                   </div>
                   <div class="number">
-                      <span>Invoice Number</span>
+                      <span class="font-weight-medium">Invoice Number</span>
+                      <span>666</span>
                   </div>
                   <div class="amount">
-                      <span>Amount</span>
+                      <span class="font-weight-medium">Amount</span>
+                      <span>400DKK</span>
                   </div>
                   <div class="status">
-                      <span>Status</span>
+                      <span class="font-weight-medium">Status</span>
+                      <span>Owned</span>
                   </div>
               </div>
           </div>
@@ -126,20 +135,17 @@ export default {};
 @import "./src/styles/styles.scss";
 
 .main {
-  width: 80%;
-  height: 100vh;
-  display: flex;
-  flex-wrap: wrap;
-  margin: 0 auto;
+  height: auto;
   margin-top: 2em;
+  @media (max-width: $sm){
+      
+  }
 
   .bot {
-      display: flex;
-      margin: 0 auto;
-
       .invoices {
-          width: 1200px;
-           margin: -2em 2em 2em;
+          min-width: 300px;
+          max-width: 1200px;
+          margin: 2em auto 2em auto;
           height: auto;
           background-color: white;
           box-shadow: 0 0 20px rgba($color: #000000, $alpha: 0.2);
@@ -152,26 +158,55 @@ export default {};
 
          .invoice-info {
              display: flex;
-             justify-content: center;
+             justify-content: space-around;
              
              .date, .number, .amount, .status {
-                 margin: 0em 6em;
+                 display: flex;
+                 flex-direction: column;
              }
          }
       }
+    @media (max-width: $md){
+        .invoices{
+            margin: 2em 1.4em 7em 1.4em;
+
+            .invoice-info{
+                flex-direction: column;
+
+                .date, .number, .amount, .status {
+                 margin: 0;
+                 flex-direction: row;
+                 justify-content: space-between;
+             }
+            }
+        }
+    }
+    @media (max-width: $xl){
+        .invoices{
+            margin: 2em 1.4em 7em 1.4em;
+        }
+    }
+    @media (min-width: 1440px) and (max-width:1580px){
+        .invoices{
+            margin: 2em 1.4em 7em 1.4em;
+        }
+    }
   }
 
   .mid {
       display: flex;
-      margin: 0 auto;
+      justify-content: center;
+      align-items: center;
+      flex-wrap: wrap;
 
       .big-box {
           background-color: white;
           box-shadow: 0 0 20px rgba($color: #000000, $alpha: 0.2);
           width: 360px;
+          min-width: 280px;
           height: 400px;
           padding: 2em;
-          margin: -2em 2em 2em;
+          margin: 2em 1% 0 1% ;
           border-radius: 10px;
 
           .block-text {
@@ -185,108 +220,132 @@ export default {};
 
           .big-title h3 {
               text-align: center;
+              margin-bottom: 10px;
           }
 
           .payment {
-              margin-left: 8em;
+              display: flex;
+              justify-content: flex-end;
 
               .payment-button {
                   background-color: map-get($ColorScheme, button);
                   color: white;
-                  padding: .5em 1em;
+                  height: 38px;
+                  width: 160px;
                   border-radius: 30px;
                   font-size: 14px;
               }
           }
 
           .change-plan{
-              margin-left: 11.5em;
-
+              display: flex;
+              justify-content: flex-end;
               .plan-button {
                   background-color: map-get($ColorScheme, button);
                   color: white;
-                  padding: .5em 1em;
+                  height: 38px;
+                  width: 106px;
                   border-radius: 30px;
                   font-size: 14px;
               }
           }
       }
-  }
 
-  .top {
-      display: flex;
-      margin: 0 auto;
+        @media (max-width: $xl){
+            .big-box{
+                width: 320px;
+
+            }
+        }
+    }
+
+    .top {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        flex-wrap: wrap;
       
-      .info-box {
-          width: 360px;
-          padding: 2em;
-          margin: 2em;
-          border-radius: 10px;
-          box-shadow: 0 0 20px rgba($color: #000000, $alpha: 0.2);
-          flex-grow: 1;
-          justify-content: center;
-          height: 140px;
-          background-color: white;
+        .info-box {
+            min-width: 320px;
+            max-width: 380px;
+            height: 140px;
+            margin: 2em 1% 0 1% ;
+            border-radius: 10px;
+            box-shadow: 0 0 20px rgba($color: #000000, $alpha: 0.2);
+            display: flex;
+            flex-direction: column;
+            justify-content: center;
+            align-items: center;
+            background-color: white;
 
-          button.get-more {
-              background-color: map-get($ColorScheme, button);
-              color: white;
-              padding: .5em;
-              border-radius: 30px;
-              font-size: 14px;
-          }
+            .info-title{
+                margin-bottom: 1em;
+            }
+            button.get-more {
+                background-color: map-get($ColorScheme, button);
+                color: white;
+                height: 38px;
+                width: 98px;
+                border-radius: 30px;
+                font-size: 14px;
+                display: flex;
+                justify-content: center;
+                align-items: center;
+            }
 
-          .moar {
-              color: white;
-          }
+            .moar {
+                color: white;
+            }
 
-          .amount {
-              font-size: 12px;
-              text-align: center;
-              padding: .3em;
-              font-weight: bold;
-          }
+            .amount {
+                font-size: 12px;
+                text-align: center;
+                padding: 0 .3em;
+                font-weight: bold;
+            }
 
-          .info-top {
-              display: flex;
-              justify-content: center;
-          }
+            .info-bot {
+                display: flex;
+                align-items: center;
 
-          .info-bot {
-              display: flex;
-              justify-content: center;
-              margin-top: -0.8em;
+                .amount, .more-button {
+                    margin: 0 1em;
+                }
 
-              .amount, .more-button {
-                  margin: 2.5em 1em;
-              }
+                .icon {
+                    margin: 0 1em;
+                }
 
-              .icon {
-                  margin: 2em 1em;
-              }
+                .icon .balance {
+                    background-color: map-get($ColorScheme, button);
+                    color: white;
+                    padding: .5em;
+                    border-radius: 100%;
+                }
 
-              .icon .balance {
-                  background-color: map-get($ColorScheme, button);
-                  color: white;
-                  padding: .5em;
-                  border-radius: 100%;
-              }
+                .icon .clip {
+                    background-color: green;
+                    color: white;
+                    padding: .5em;
+                    border-radius: 100%;
+                }
 
-              .icon .clip {
-                  background-color: green;
-                  color: white;
-                  padding: .5em;
-                  border-radius: 100%;
-              }
+                .icon .external {
+                    background-color: blue;
+                    color: white;
+                    padding: .5em;
+                    border-radius: 100%;
+                }
+            }
+        }
+        @media (max-width: $sm){
+            flex-direction: column;
 
-              .icon .external {
-                  background-color: blue;
-                  color: white;
-                  padding: .5em;
-                  border-radius: 100%;
-              }
-          }
-      }
-  }
+            .info-box{
+                margin: 2em auto 0 auto ;
+            }
+        }
+
+    }
 }
 </style>
