e798283760514cf115143b16c61e62a5dd2f72c9
BenjaminKing1337 <BenjaminKing1337@users.noreply.github.com>
2021-05-10 12:45:50 +0200

    corrections

--- src/views/MainFeatures/Cardview.vue
+++ src/views/MainFeatures/Cardview.vue
@@ -1,29 +1,25 @@
 <template>
+  <div>
     <div id="cardview">
-        <div id="filter"></div>
-        <div id="cards"></div>
+      <div id="filter"></div>
+      <div id="cards"></div>
     </div>
+  </div>
 </template>
 
 <script>
-    export default {
-        
-    }
+export default {};
 </script>
 
 <style lang="scss" scoped>
 @import "./src/styles/styles.scss";
 
 #cardview {
-    background-color: map-get($cs, white1);
-    height: 100vh;
-    width: auto;
-    #filter {
-
-    }
-    #cards {
-
-    }
+  height: 100vh;
+  width: auto;
+  #filter {
+  }
+  #cards {
+  }
 }
-
 </style>
